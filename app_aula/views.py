10eae114ebc78733e673d9b1695c524348ee68f8
Arthur Medeiros <afm.arthur@gmail.com>
2024-03-25 02:18:48 -0300

    first commitX

---
+++ app_aula/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def func1(request):
+    return render(request,"app_aula/index.html")
+
+
+def sobre(request):
+    return render(request,"app_aula/sobre.html")
+
+def discografia(request):
+    return render(request,"app_aula/discografia.html")    
